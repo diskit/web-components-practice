76af586c38ded9e558335f48b8652aa585b20113
diskit <diskit.1980@gmail.com>
2021-03-11 08:25:21 +0900

    整理

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+div {
+  color: var(--text-color, #ffa500);
+}
+</style>
+<div class="message"></div>
+<slot name="name">default name</slot>
+`;
+
 export class GreetingElement extends HTMLElement {
   constructor() {
     super();
-    const template = document.querySelector('#greeting-template') as HTMLTemplateElement;
     const shadowRoot = this.attachShadow({mode: "open"});
-    shadowRoot.appendChild(template.content.cloneNode(true));
+    const content = template.content.cloneNode(true);
+    const message = this.getAttribute('message');
+    (content as DocumentFragment).querySelector('.message')!.textContent = message;
+    shadowRoot.appendChild(content);
   }
 }
 
 customElements.define('x-greeting', GreetingElement);
+
